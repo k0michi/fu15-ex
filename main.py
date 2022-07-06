0549da14cf9ebfd2f90d47c964e0f9317e2acef5
Hajime Fukai <s1290102@u-aizu.ac.jp>
2022-07-06 13:56:34 +0900

    HoT: initial revision

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+print("Tossing a coin...")
+
+hCount = 0
+tCount = 0
+
+for i in range(1, 4):
+  r = random.randint(0, 1)
+
+  if r == 0:
+    print(f"Round {i}: Heads")
+    hCount += 1
+  else:
+    print(f"Round {i}: Tails")
+    tCount += 1
+
+print(f"Heads: {hCount}, Tails: {tCount}")
